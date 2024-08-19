d90b09806f7250fde22d7964ed932de03879eebb
Bruno Santos <80181592+Brunoss21@users.noreply.github.com>
2024-08-19 20:47:00 -0300

    Create Palindromo.js

---
+++ Palindromo.js
@@ -0,0 +1,15 @@
+function palindromo(){
+
+    let palavra = "Hanah";
+
+    let palinDromo = "Hanah";
+
+    if(palavra === palinDromo){
+        palavra.reverse;
+        console.log("Essa palavra é palindrome");
+    } else{
+        console.log("Essa palavra não é palindromo");
+    }
+
+    console.log(palavra);
+}
